7e76c69c180d451389f036bdb49cc865ea1d3d79
flyingpie101 <flyingpie101@users.noreply.github.com>
2015-07-12 11:27:06 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Greg Beck on 2015-07-12.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
