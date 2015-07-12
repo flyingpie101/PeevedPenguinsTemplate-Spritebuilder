94b8114ed2c3bbf3ef3040071dd80893b15168ec
flyingpie101 <flyingpie101@users.noreply.github.com>
2015-07-12 19:05:35 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -17,6 +17,9 @@
     CCNode *_contentNode;
     
     CCNode *_pullbackNode;
+    
+    CCnode *_mouseJointNode;
+    CCPhysicsJoint *_mouseJoint;
 }
 
 //is called when CCB file has completed loading
@@ -29,16 +32,58 @@
     [_levelNode addChild:level];
     
     //visualize physics bodies and joints
-    _physicsNode.debugDraw = TRUE;
+    //_physicsNode.debugDraw = TRUE;
     
     //nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
+    _mouseJointNode.physicsBody.collisionMask = @[];
+    
 }
 
 //called on every touch in this scene
 -(void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
-    [self launchPenguin];
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    //start catapult dragging when a touch inside of the catapult arm occurs
+    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
+    {
+        // move the mouseJointNode to the touch position
+        _mouseJointNode.position = touchLocation;
+        
+        //setup a spring joint between the mouseJointNode and the catapultArm
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0,0) anchorB:ccp(34,138) restLength:0.f stiffness:3000.f damping:150.f];
+    }
+    
+}
+
+-(void) releaseCatapult
+{
+    if (_mouseJoint != nil)
+    {
+        //releases the joint and lets the catapult snap back
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+    }
+}
+
+-(void) touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    //when touches end, meaning the user releases their finger, release the catapult
+    [self releaseCatapult];
+}
+
+-(void) touchCancelled:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    //when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
+    [self releaseCatapult];
+}
+
+- (void) touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    //whenever touches move, update the position of the mouseJointNode
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
 }
 
 -(void)launchPenguin
